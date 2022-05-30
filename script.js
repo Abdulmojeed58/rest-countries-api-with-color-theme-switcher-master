a3db2b328e549f0e97aa495fd604a7d2349c1883
Abdulmojeed Ayoola <mojeedayoola58@gmail.com>
2022-05-30 12:13:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const body = $("body")
 const filterBtn = $(".region .button")
 const list = $(".region .list")
 const input = $('[data-input]')
-// console.log(input)
+const element = $('.countries')
+const regionList = $$('ul li')
+// console.log(regionList)
 
+
+// //////TOGGLE LIGHT AND DARK MODE ///////
 $('.dark-light').addEventListener("click", ()=>{
     body.classList.toggle('light-mode')
 })
@@ -25,17 +29,15 @@ filterBtn.addEventListener("click", ()=>{
 
 
 
-const element = $('.countries')
-input.addEventListener('keyup', fetchCountryData)
-const countries = []
+//////FETCH DATA FROM COUNTRY API ///////////
 async function fetchCountryData(){
     const res = await fetch("https://restcountries.com/v2/all?fields=name,capital,currencies,population,region,subregion,topleveldomain,nativename,flag")
     const data = await res.json()
-    doSomething(data, this.value)
+    doSomething(data, this.value, this.textContent)
 }
 
-function doSomething(result, wordToMatch){
-    let m = filt(result, wordToMatch)
+function doSomething(result, wordToMatch, region){
+    let m = filt(result, wordToMatch, region)
     let rest = m.map(res=>{
         return `
         <div class="card country">
@@ -56,11 +58,16 @@ function doSomething(result, wordToMatch){
     element.innerHTML = rest
 }
 
-function filt(result, wordToMatch){
+function filt(result, wordToMatch, region){
     return result.filter(res=>{
         let regex = new RegExp(wordToMatch, 'gi')
-        return res.name.match(regex)
+        return res.name.match(regex) || res.region == region
     })
 }
-// fetchCountryData()
 
+
+
+
+
+regionList.forEach(list => list.addEventListener('click', fetchCountryData));
+input.addEventListener('keyup', fetchCountryData)
